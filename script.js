084ab04540df1a8f14ef46ca03ebaf57db12a477
jridyard <85185061+jridyard@users.noreply.github.com>
2022-08-28 11:10:00 -0500

    minified scriptjs

--- script.js
+++ script.js
@@ -1,158 +1,279 @@
-const testing = 'http://localhost:5000'
-const production = 'https://www.ticketdecisionassistant.com'
-globalThis.environment = production
-
-window.addEventListener('DOMContentLoaded', async (e) => {
-
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-right',
-        iconColor: 'white',
-        customClass: {
-            popup: 'colored-toast'
-        },
-        showConfirmButton: false,
-        timer: 2300
-    })
-
-    document.querySelector('#confirm-code').addEventListener('click', async () => {
-
-        const alreadyRegisteredCheckRaw = await chrome.storage.local.get('registration')
-        const alreadyRegisteredCheck = alreadyRegisteredCheckRaw['registration_code']
-        if (alreadyRegisteredCheck) {
-            await Toast.fire({ icon: 'error', title: 'You have already registered an access code from this extension.' })
-            return
-        }
-
-        let activation_code = document.querySelector('#activation_code').value
-
+function _0x51b855(_0x5bc1dc, _0x5918c4) {
+    return _0x8449(_0x5918c4 - 0x105, _0x5bc1dc);
+}
+function _0x8449(_0x35342d, _0x4804ac) {
+    const _0x2261c9 = _0xf5f5();
+    _0x8449 = function (_0x3f2828, _0x4ea639) {
+        _0x3f2828 = _0x3f2828 - 0xec;
+        let _0x12d199 = _0x2261c9[_0x3f2828];
+        return _0x12d199;
+    };
+    return _0x8449(_0x35342d, _0x4804ac);
+}
+(function (_0x4ab11d, _0x4c6740) {
+    function _0x4d1d7d(_0x910b30, _0x5c99a5) {
+        return _0x8449(_0x5c99a5 - -0x38f, _0x910b30);
+    }
+    const _0x31cf70 = _0x4ab11d();
+    while (!![]) {
         try {
-            const authorization_status = await fetch(`${globalThis.environment}/api/activate_purchaser_account`, {
-                "method": "POST",
-                "body": JSON.stringify({
-                    'activation_code': activation_code.toString()
-                }),
-                cache: "no-cache",
-                headers: new Headers({
-                    "content-type": "application/json"
-                })
-            }).then((response) => response.json())
-            .then((data) => {
-                return data
-            })
-            .catch(err => {
-                console.error(err)
-                return {'response': 'Unkown error occured. Please contact an admin.'}
-            })
-
-            console.log(authorization_status)
-
-            if (authorization_status['response'] == "Success") {
-                chrome.storage.local.set({
-                    'registration' : authorization_status['registration_code'],
-                    'username': authorization_status['username'],
-                    'activation_status': authorization_status['activation_status'],
-                    'activation_code': activation_code
-                })
-                document.querySelector('#username').textContent =  authorization_status['username']
-                document.querySelector('#activation_status').textContent = authorization_status['activation_status'] == "True" ? "Active" : "Inactive"
-                document.querySelector('#pre').style.display = 'none'
-                document.querySelector('#post').style.display = 'block'
-
-                await Toast.fire({ icon: 'success', title: 'Your account has been set up!' })
-
-                return
+            const _0x1b8c4b = parseInt(_0x4d1d7d(-0x2ac, -0x299)) / 0x1 * (parseInt(_0x4d1d7d(-0x2ac, -0x2a3)) / 0x2) + parseInt(_0x4d1d7d(-0x293, -0x29e)) / 0x3 + parseInt(_0x4d1d7d(-0x2a2, -0x288)) / 0x4 + parseInt(_0x4d1d7d(-0x26a, -0x27c)) / 0x5 * (parseInt(_0x4d1d7d(-0x268, -0x26d)) / 0x6) + parseInt(_0x4d1d7d(-0x273, -0x27f)) / 0x7 * (-parseInt(_0x4d1d7d(-0x2ab, -0x298)) / 0x8) + -parseInt(_0x4d1d7d(-0x287, -0x270)) / 0x9 * (parseInt(_0x4d1d7d(-0x2a1, -0x291)) / 0xa) + -parseInt(_0x4d1d7d(-0x29a, -0x28d)) / 0xb;
+            if (_0x1b8c4b === _0x4c6740) {
+                break;
+            } else {
+                _0x31cf70['push'](_0x31cf70['shift']());
             }
-            
-            await Toast.fire({ icon: 'error', title: authorization_status['response'] })
+        } catch (_0x32e0e5) {
+            _0x31cf70['push'](_0x31cf70['shift']());
         }
-        catch (err) {
-            console.error(err)
-            await Toast.fire({ icon: 'error', title: 'Unknown error occured. Please try again or contact an admin.' })
-            return 'Unkown error occured. Please contact an admin.'
+    }
+}(_0xf5f5, 0x50972));
+const _0x4ea639 = (function () {
+    let _0x5163bb = !![];
+    return function (_0x5e3b0c, _0x5263bf) {
+        const _0x163606 = _0x5163bb ? function () {
+            if (_0x5263bf) {
+                const _0x55fbcf = _0x5263bf['apply'](_0x5e3b0c, arguments);
+                _0x5263bf = null;
+                return _0x55fbcf;
+            }
+        } : function () {
+        };
+        _0x5163bb = ![];
+        return _0x163606;
+    };
+}());
+const _0x3f2828 = _0x4ea639(this, function () {
+    let _0x250bb4;
+    function _0x5536f9(_0xbf5f46, _0x58bd10) {
+        return _0x8449(_0x58bd10 - -0x326, _0xbf5f46);
+    }
+    try {
+        const _0x2579cd = Function('return\x20(function()\x20' + '{}.constructor(\x22return\x20this\x22)(\x20)' + ');');
+        _0x250bb4 = _0x2579cd();
+    } catch (_0x8966bc) {
+        _0x250bb4 = window;
+    }
+    const _0x57ddf3 = _0x250bb4['console'] = _0x250bb4[_0x5536f9(-0x21d, -0x21b)] || {};
+    const _0xb683fd = [
+        'log',
+        'warn',
+        _0x5536f9(-0x224, -0x220),
+        'error',
+        'exception',
+        'table',
+        'trace'
+    ];
+    for (let _0x520e95 = 0x0; _0x520e95 < _0xb683fd['length']; _0x520e95++) {
+        const _0x1b0a1b = _0x4ea639['constructor'][_0x5536f9(-0x204, -0x219)]['bind'](_0x4ea639);
+        const _0x38446d = _0xb683fd[_0x520e95];
+        const _0x16f8b6 = _0x57ddf3[_0x38446d] || _0x1b0a1b;
+        _0x1b0a1b['__proto__'] = _0x4ea639['bind'](_0x4ea639);
+        _0x1b0a1b['toString'] = _0x16f8b6['toString']['bind'](_0x16f8b6);
+        _0x57ddf3[_0x38446d] = _0x1b0a1b;
+    }
+});
+_0x3f2828();
+function _0xf5f5() {
+    const _0x1574d3 = [
+        '551321bJILtz',
+        '12968wVBlxH',
+        '#activation_status',
+        'set',
+        'Inactive',
+        'Unkown\x20error\x20occured.\x20Please\x20contact\x20an\x20admin.',
+        'environment',
+        'block',
+        '10WlTRAe',
+        'True',
+        'json',
+        'click',
+        '5529029OnACCk',
+        'display',
+        '#loader',
+        'Success',
+        'info',
+        '2631388sTKmfY',
+        'ssddsdss',
+        'mixin',
+        'storage',
+        'console',
+        'fire',
+        'prototype',
+        'http://localhost:5000',
+        'then',
+        '2513qPFhQz',
+        'username',
+        '#account_removed',
+        '5WTJsSc',
+        '#username',
+        'get',
+        'Your\x20account\x20has\x20been\x20set\x20up!',
+        'response',
+        'querySelector',
+        'style',
+        'Security',
+        'Unknown\x20error\x20occured.\x20Please\x20try\x20again\x20or\x20contact\x20an\x20admin.',
+        '#activation_code',
+        'stringify',
+        '#confirm-code',
+        '4060431snUSml',
+        '/api/check_user_status',
+        'success',
+        '1927974LLeaaP',
+        'no-cache',
+        '2tDtbfr',
+        'reason',
+        'log',
+        '#pre',
+        '#post',
+        '1006014CKTXpl',
+        'registration_code',
+        'deactivated',
+        'addEventListener',
+        'textContent'
+    ];
+    _0xf5f5 = function () {
+        return _0x1574d3;
+    };
+    return _0xf5f5();
+}
+const testing = _0x51b855(0x206, 0x213);
+const production = 'https://www.ticketdecisionassistant.com';
+globalThis[_0x51b855(0x204, 0x201)] = production;
+window[_0x51b855(0x208, 0x1f9)]('DOMContentLoaded', async _0x58cbfa => {
+    const _0x5b5e13 = Swal[_0x353202(0x2d6, 0x2c1)]({
+        'toast': !![],
+        'position': 'top-right',
+        'iconColor': 'white',
+        'customClass': { 'popup': 'colored-toast' },
+        'showConfirmButton': ![],
+        'timer': 0x8fc
+    });
+    document['querySelector'](_0x353202(0x2ee, 0x2d6))['addEventListener'](_0x353202(0x2ce, 0x2b9), async () => {
+        const _0x188ab1 = await chrome['storage']['local']['get']('registration');
+        const _0x28f8aa = _0x188ab1[_0x5bac7f(0xeb, 0x101)];
+        if (_0x28f8aa) {
+            await _0x5b5e13['fire']({
+                'icon': 'error',
+                'title': 'You\x20have\x20already\x20registered\x20an\x20access\x20code\x20from\x20this\x20extension.'
+            });
+            return;
         }
-
-    })
-        
-    let user_token_raw = await chrome.storage.local.get([
-        'activation_code'
-    ])
-    let user_token = user_token_raw['activation_code']
-
-    if (user_token) {
-        const authorization_status = await fetch(`${globalThis.environment}/api/check_user_status`, {
-            "method": "POST",
-            "body": JSON.stringify({
-                'activation_code': user_token.toString()
-            }),
-            cache: "no-cache",
-            headers: new Headers({
-                "content-type": "application/json"
-            })
-        }).then((response) => response.json())
-        .then((data) => {
-            return data
-        })
-
-        console.log(authorization_status)
-    
-        if (authorization_status.response == 'deactivated') {
-            chrome.storage.local.set({
-                'deactivated': true,
-                'reason': authorization_status.reason // "Security" vs "Deleted By User"
-            })
+        function _0x5bac7f(_0x57dc7e, _0x457daa) {
+            return _0x353202(_0x57dc7e, _0x457daa - -0x1a9);
+        }
+        let _0x3dc727 = document['querySelector'](_0x5bac7f(0x134, 0x12b))['value'];
+        try {
+            const _0x345a7d = await fetch(globalThis['environment'] + '/api/activate_purchaser_account', {
+                'method': 'POST',
+                'body': JSON['stringify']({ 'activation_code': _0x3dc727['toString']() }),
+                'cache': 'no-cache',
+                'headers': new Headers({ 'content-type': 'application/json' })
+            })[_0x5bac7f(0x129, 0x11e)](_0x76783a => _0x76783a[_0x5bac7f(0xf9, 0x10f)]())[_0x5bac7f(0x122, 0x11e)](_0x22ca1b => {
+                return _0x22ca1b;
+            })['catch'](_0x487eb6 => {
+                console['error'](_0x487eb6);
+                return { 'response': 'Unkown\x20error\x20occured.\x20Please\x20contact\x20an\x20admin.' };
+            });
+            console['log'](_0x345a7d);
+            if (_0x345a7d['response'] == _0x5bac7f(0x109, 0x114)) {
+                chrome['storage']['local']['set']({
+                    'registration': _0x345a7d['registration_code'],
+                    'username': _0x345a7d['username'],
+                    'activation_status': _0x345a7d['activation_status'],
+                    'activation_code': _0x3dc727
+                });
+                document[_0x5bac7f(0x125, 0x127)](_0x5bac7f(0x13a, 0x123))['textContent'] = _0x345a7d['username'];
+                document[_0x5bac7f(0x140, 0x127)]('#activation_status')['textContent'] = _0x345a7d['activation_status'] == _0x5bac7f(0x109, 0x10e) ? 'Active' : _0x5bac7f(0x112, 0x109);
+                document['querySelector']('#pre')['style']['display'] = 'none';
+                document['querySelector']('#post')['style']['display'] = _0x5bac7f(0x11c, 0x10c);
+                await _0x5b5e13['fire']({
+                    'icon': _0x5bac7f(0x141, 0x130),
+                    'title': _0x5bac7f(0x114, 0x125)
+                });
+                return;
+            }
+            await _0x5b5e13['fire']({
+                'icon': 'error',
+                'title': _0x345a7d[_0x5bac7f(0x11e, 0x126)]
+            });
+        } catch (_0x254864) {
+            console['error'](_0x254864);
+            await _0x5b5e13[_0x5bac7f(0x125, 0x11b)]({
+                'icon': 'error',
+                'title': _0x5bac7f(0x129, 0x12a)
+            });
+            return _0x5bac7f(0x120, 0x10a);
+        }
+    });
+    let _0xf5ddf4 = await chrome[_0x353202(0x2b3, 0x2c2)]['local']['get'](['activation_code']);
+    let _0x230385 = _0xf5ddf4['activation_code'];
+    if (_0x230385) {
+        const _0x5eab3f = await fetch(globalThis['environment'] + _0x353202(0x2df, 0x2d8), {
+            'method': 'POST',
+            'body': JSON[_0x353202(0x2df, 0x2d5)]({ 'activation_code': _0x230385['toString']() }),
+            'cache': _0x353202(0x2de, 0x2db),
+            'headers': new Headers({ 'content-type': 'application/json' })
+        })['then'](_0x376c3a => _0x376c3a['json']())['then'](_0x22ee8a => {
+            return _0x22ee8a;
+        });
+        console[_0x353202(0x2a3, 0x2a6)](_0x5eab3f);
+        if (_0x5eab3f[_0x353202(0x2dd, 0x2cf)] == 'deactivated') {
+            chrome['storage']['local'][_0x353202(0x2a1, 0x2b1)]({
+                'deactivated': !![],
+                'reason': _0x5eab3f[_0x353202(0x2a5, 0x2a5)]
+            });
         } else {
-            chrome.storage.local.set({
-                'deactivated': false,
+            chrome[_0x353202(0x2b4, 0x2c2)]['local'][_0x353202(0x299, 0x2b1)]({
+                'deactivated': ![],
                 'reason': null,
-                'activation_status': authorization_status.activation_status
-            })
+                'activation_status': _0x5eab3f['activation_status']
+            });
         }
     } else {
-        document.querySelector('#loader').style.display = 'none'
-        document.querySelector('#pre').style.display = 'block'
-        return
+        document[_0x353202(0x2e9, 0x2d0)]('#loader')['style']['display'] = 'none';
+        document['querySelector']('#pre')[_0x353202(0x2b5, 0x2d1)][_0x353202(0x2b1, 0x2bb)] = 'block';
+        return;
     }
-
-
-    let deactivation_data = await chrome.storage.local.get([
-        'deactivated', 'reason'
-    ])
-    let deactivated = deactivation_data['deactivated']
-    let reason = deactivation_data['reason']
-
-    if (deactivated) {
-        document.querySelector('#loader').style.display = 'none'
-        document.querySelector('#pre').style.display = 'none'
-        document.querySelector('#post').style.display = 'block'
-        document.querySelector('#account_active').style.display = 'none'
-        if (reason == 'Security') {
-            document.querySelector('#account_inactive_security').style.display = 'block'
+    let _0x5f01a3 = await chrome['storage']['local'][_0x353202(0x2bd, 0x2cd)]([
+        _0x353202(0x2c5, 0x2ab),
+        'reason'
+    ]);
+    let _0x13b8ab = _0x5f01a3['deactivated'];
+    let _0x4b8b5a = _0x5f01a3['reason'];
+    if (_0x13b8ab) {
+        document[_0x353202(0x2ce, 0x2d0)](_0x353202(0x2bc, 0x2bc))['style'][_0x353202(0x2a1, 0x2bb)] = 'none';
+        document['querySelector']('#pre')[_0x353202(0x2e0, 0x2d1)]['display'] = 'none';
+        document['querySelector'](_0x353202(0x2be, 0x2a8))['style']['display'] = 'block';
+        document[_0x353202(0x2b9, 0x2d0)]('#account_active')[_0x353202(0x2e9, 0x2d1)]['display'] = 'none';
+        if (_0x4b8b5a == _0x353202(0x2cb, 0x2d2)) {
+            document['querySelector']('#account_inactive_security')['style']['display'] = 'block';
         } else {
-            document.querySelector('#account_removed').style.display = 'block'
+            document['querySelector'](_0x353202(0x2c0, 0x2ca))['style']['display'] = 'block';
         }
-        console.log('ssddsdss')
-        return
+        console['log'](_0x353202(0x2a6, 0x2c0));
+        return;
     }
-
-    console.log('hitting here')
-
-    let data = await chrome.storage.local.get([
-        'username', 'activation_status'
-    ])
-    let user_name = data['username']
-    let activation_status = data['activation_status']
-
-    console.log('hwere')
-    if (user_name) {
-        document.querySelector('#loader').style.display = 'none'
-        document.querySelector('#username').textContent = user_name
-        document.querySelector('#activation_status').textContent = activation_status == "True" ? "Active" : "Inactive"
-        document.querySelector('#pre').style.display = 'none'
-        document.querySelector('#post').style.display = 'block'
+    function _0x353202(_0x18c511, _0x2ed420) {
+        return _0x51b855(_0x18c511, _0x2ed420 - 0xb3);
+    }
+    console['log']('hitting\x20here');
+    let _0x4be901 = await chrome['storage']['local'][_0x353202(0x2b6, 0x2cd)]([
+        'username',
+        'activation_status'
+    ]);
+    let _0xb3e3d8 = _0x4be901[_0x353202(0x2be, 0x2c9)];
+    let _0xb7b5e0 = _0x4be901['activation_status'];
+    console[_0x353202(0x2a4, 0x2a6)]('hwere');
+    if (_0xb3e3d8) {
+        document['querySelector'](_0x353202(0x2c9, 0x2bc))[_0x353202(0x2c2, 0x2d1)]['display'] = 'none';
+        document['querySelector']('#username')[_0x353202(0x2c6, 0x2ad)] = _0xb3e3d8;
+        document[_0x353202(0x2d5, 0x2d0)](_0x353202(0x2b2, 0x2b0))['textContent'] = _0xb7b5e0 == 'True' ? 'Active' : _0x353202(0x29d, 0x2b2);
+        document['querySelector']('#pre')['style'][_0x353202(0x2c6, 0x2bb)] = 'none';
+        document[_0x353202(0x2d2, 0x2d0)](_0x353202(0x2b2, 0x2a8))['style']['display'] = 'block';
     } else {
-        document.querySelector('#loader').style.display = 'none'
-        document.querySelector('#pre').style.display = 'block'
+        document['querySelector'](_0x353202(0x2b4, 0x2bc))['style'][_0x353202(0x2d3, 0x2bb)] = 'none';
+        document[_0x353202(0x2e3, 0x2d0)](_0x353202(0x2bf, 0x2a7))[_0x353202(0x2c8, 0x2d1)]['display'] = _0x353202(0x2c2, 0x2b5);
     }
-
-})
+});
